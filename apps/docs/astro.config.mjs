962716143323dea7fc026a2102d9e6aad4f5fc4a
yyyoichi <git@yyyoichi.com>
2026-03-09 05:25:24 +0000

    add: deploy page action

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yyyoichi.github.io",
+  base: "/chrono-kinesis",
   trailingSlash: "never",
   integrations: [
     starlight({
