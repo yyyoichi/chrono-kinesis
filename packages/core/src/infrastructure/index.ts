5fc08bb281be82aad1c12d2928a4d7e0df5f0f0c
yyyoichi <git@yyyoichi.com>
2026-03-31 06:27:38 +0000

    add: export intersection

--- packages/core/src/infrastructure/index.ts
+++ packages/core/src/infrastructure/index.ts
@@ -1,6 +1,7 @@
 export * from "../domain/clock";
 export * from "./clock-dom";
 export * from "./dialog-dom";
+export * from "./intersection-dom";
 export * from "./physics-dom";
 export * from "./rect-dom";
 export * from "./source-dom";
