ebbba4834eca5bccff7bff7c087f2d8793b13a0f
yyyoichi <git@yyyoichi.com>
2026-05-05 08:18:37 +0000

    ref: rename application -> simulator

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./adaptors";
-export * from "./application";
 export * from "./chronos";
 export * from "./kinesis";
 export * from "./ports";
+export * from "./simulator";
 export * from "./state";
